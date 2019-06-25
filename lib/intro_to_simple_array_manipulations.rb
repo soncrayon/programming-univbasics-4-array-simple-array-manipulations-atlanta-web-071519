d63c81c4172f16b9d1786e47d52031eb97b55041
Curtis Arnold <arnold.curtis@gmail.com>
2019-06-25 01:24:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,6 +21,16 @@ end
 def using_concat(array1, array2) 
   array1.concat(array2)
 end 
+
+def using_insert(array, new_element) 
+  array.insert[4] 
+end
+
+def using_uniq(array) 
+  array.uniq 
+end
+
+
   
   
   
